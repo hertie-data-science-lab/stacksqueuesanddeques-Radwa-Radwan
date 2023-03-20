e0f74875c9b7502c48c8ff5e1a63b3d73812cb82
Radwa-Radwan <112707526+Radwa-Radwan@users.noreply.github.com>
2023-03-20 08:20:38 +0200

    update_main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from code_file import ArrayDeque
 
 D = ArrayDeque()
+
 for i in range(D.DEFAULT_CAPACITY):
     D.add_last(i)
     print(len(D), D.elements)
+
 for i in range(10):
     D.delete_first()
     print(len(D), D.elements)
